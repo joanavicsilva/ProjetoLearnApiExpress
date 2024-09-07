d7e5327d8f4747067d0a067dcf689d2959b497c5
joanavicsilva <164263550+joanavicsilva@users.noreply.github.com>
2024-09-07 09:10:23 -0300

    Update category.route.js

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -9,11 +9,69 @@ router.get('', (req, res) => {
 })
 
 //rota para criar uma categoria
+router.post('/:categoryName', async (req,res) => {
+    try{
+        const { categoryName } = req.params;
+
+
+        const newCategory = await prisma.category.create({
+            data: {
+                name: categoryName
+            }
+        })
+
+        res.json(newCategory)
+    }catch (err){
+        console.log(err)
+        res.status(500).send(err)
+    }
+})
 
 //rota para deletar uma categoria
+router.delete('/:categoryName', async (req,res) => {
+    try{
+        const {categoryName} = req.params
+
+        const category = await prisma.category.delete({
+            where: {
+                name: categoryName
+            }
+        })
+
+        res.json(category)
+    }catch(err){
+        console.log(err)
+        res.status(500).send(err)
+    }
+})
 
 //rota para listar todas as categorias
+router.get('/all', async (req, res) => {
+    try {
+        const {category} = await prisma.category.findMany();
+        res.json(category);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
 
 //rota listar todos os blogs de uma categoria
+router.get('/search/:categoryName', async (req, res) => {
+    try {
+        const {categoryName} = req.params;
+
+        const blogs = await prisma.category.findMany({
+            where: {
+                category_name: {contains: categoryName}
+            }
+        })
+
+        res.json(blogs);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
 
 export default router;
